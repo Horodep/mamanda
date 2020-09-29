450dfb51110686ec1612aaace84d6ff19ceb9bf5
Horodep <Horodep@yandex.ru>
2020-09-29 16:57:56 +0300

    index good

--- index.js
+++ index.js
@@ -7,11 +7,31 @@ client.login(config.discordApiKey);
 
 
 client.on('ready', () => {
-
-});
-
-client.on("message", function(message) {
-	if (message.author.bot) return;
+	console.log('Ready to master!'); 
+	
+	var date = new Date();
+	var startDate = new Date();
+	lastDate = new Date();
+	
+	var repeateAction = function(){ 
+		date = new Date(); 
+		
+		if(date - lastDate < 58000 && date - startDate > 60000){
+			console.log(date, "hardreset", date - startDate, date - lastDate);
+			channel_sandbox = client.channels.cache.get(config.channels.sandbox);
+			channel_sandbox.send((date - lastDate) + " миллисекунд");
+			setTimeout(function(){
+				var shiza = client.find();
+			}, 150);
+		}else if(date.getMinutes() === 0 && date.getHours() === 5 && date - startDate > 60000){
+			console.log(date, "destroy", date - startDate, date - lastDate);
+			client.destroy().then(() => client.login(keys.discordApiKey()));
+		}else{
+			setTimeout(repeateAction, 1000*60); 
+		}
+		lastDate = date;
+	} 
+	repeateAction();
 });
 
 client.on('guildMemberAdd', member => {
@@ -19,29 +39,17 @@ client.on('guildMemberAdd', member => {
 	console.log('NEW MEMBER ' + member.displayName); 
 });
 
+var message = require('./message');
+client.on("message", function(_message) => message.main(_message));
+
 var messageReactionAdd = require('./messageReactionAdd');
 client.on('messageReactionAdd', (reaction, user) => messageReactionAdd.main(reaction, user));
 
 var messageReactionRemove = require('./messageReactionRemove');
 client.on('messageReactionRemove', (reaction, user) => messageReactionRemove.main(reaction, user));
 
-client.on("messageDelete", (messageDelete) => {
-	console.log("messageDeleted");
-	try{
-		if(!messageDelete.author.bot &&
-			messageDelete.channel.id != '626432384643891220' &&
-			messageDelete.channel.id != '526145050871332924'){
-			channel_deleted = client.channels.get("751373050330611733");
-			console.log(messageDelete.content, messageDelete.content.split("@").join(""));
-			channel_deleted.send("<@"+messageDelete.author.id+">: "+messageDelete.content.split("@").join(""));
-			for (var value of messageDelete.attachments.values()) {
-				channel_deleted.send("Вложение:", {files: [value.proxyURL]});
-			}
-		}
-	} catch(e) {
-//		channel_sandbox.send('Ошибка ' + e.name + ":" + e.message + "\n<@149245139389251584> \n" + e.stack);
-	}
-});
+var messageDelete = require('./messageDelete');
+client.on("messageDelete", (message) => messageDelete.main(message));
 
 client.on('raw', async event => {
 	try{
@@ -50,23 +58,23 @@ client.on('raw', async event => {
 		console.log(event.t);
 		const { d: data } = event;
 		
-		const user = client.users.get(data.user_id);
-		const channel = client.channels.get(data.channel_id) || await user.createDM();
+		const user = client.users.cache.get(data.user_id);
+		const channel = client.channels.cache.get(data.channel_id) || await user.createDM();
 
-		if (channel.messages.has(data.message_id)) return;
-		const message = await channel.fetchMessage(data.message_id);
+		const message = await channel.messages.fetch(data.message_id);
+		if (message == null) return;
 		
 		const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
-		let reaction = message.reactions.get(emojiKey);
+		let reaction = message.reactions.cache.get(emojiKey);
 
 		if (!reaction) {
-			const emoji = new Discord.Emoji(client.guilds.get(data.guild_id), data.emoji);
+			const emoji = new Discord.Emoji(client.guilds.cache.get(data.guild_id), data.emoji);
 			reaction = new Discord.MessageReaction(message, emoji, 1, data.user_id === client.user.id);
 		}
 
 		client.emit(events[event.t], reaction, user);
 	} catch(e) {
-		message.channel.send('Ошибка ' + e.name + ":" + e.message + "\n<@149245139389251584> \n" + e.stack);
+		require('./catcherror').catcherror(e, client);
 	}
 });
 
