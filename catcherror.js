5d201e7275154a18ba9dd6ee9f6eec06c00e8bf2
Horodep <horodep@yandex.ru>
2020-10-21 11:25:39 +0300

    error user id ping fix

--- catcherror.js
+++ catcherror.js
@@ -4,5 +4,5 @@ const config = require("./config.json");
 exports.catcherror = function(e, client) {
 	console.log("error " + e.name + ":" + e.message);
 	channel_sandbox = client.channels.cache.get(config.channels.sandbox);
-	channel_sandbox.send('Ошибка ' + e.name + ":" + e.message + "\n<@" + config.users.boss + "> \n" + e.stack);
+	channel_sandbox.send('Ошибка ' + e.name + ":" + e.message + "\n<@" + config.users.developer + "> \n" + e.stack);
 }
