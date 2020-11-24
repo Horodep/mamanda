5c4b4080110893dc5bb964265807caf397cd8cfe
Horodep <horodep@yandex.ru>
2020-11-25 00:27:42 +0300

    ExecuteForEveryMember

--- clan.js
+++ clan.js
@@ -13,8 +13,8 @@ function getFullMemberData(membershipType, membershipId){
 
 function getAllMembers(){
 	var members = [];
-	Array.prototype.push.apply(members, getClanMembers(config.clans.clan1));
-	Array.prototype.push.apply(members, getClanMembers(config.clans.clan2));
+	Array.prototype.push.apply(members, getClanMembers(config.clans[0]));
+	Array.prototype.push.apply(members, getClanMembers(config.clans[1]));
 	return members;
 }
 
@@ -28,77 +28,20 @@ export function FindMemberByFullName(fullName) {
 	});
 }
 
-export function ClanTime(message) {
+export function ExecuteForEveryMember(timeout, callback) {
 	var members = getAllMembers();
-	//.....
-}
-export function bruteforce (){
-	console.log("Check in Penumbra;");
-	var clan = new XMLHttpRequest();
-	clan.open("GET", "https://www.bungie.net/Platform/GroupV2/3055823/Members/", true);
-	clan.setRequestHeader("X-API-Key", d2apiKey);
-	clan.onreadystatechange = function(){
-		if(this.readyState === 4 && this.status === 200){
-			var json = JSON.parse(this.responseText);
-		
-			var found_member = null;
-			var members = json.Response.results;
-			members.forEach(function(member) { 
-				if(d_member.displayName.startsWith(member.destinyUserInfo.LastSeenDisplayName + " ") || d_member.displayName == member.destinyUserInfo.LastSeenDisplayName){
-					found_member = member;
-				}
-			});
-			
-			if(found_member != null){
-				console.log(found_member.destinyUserInfo.LastSeenDisplayName);
-				if (sync == true) CreateSync(channel, discord_id, found_member.destinyUserInfo.membershipType, found_member.destinyUserInfo.membershipId, found_member.destinyUserInfo.LastSeenDisplayName);
-				roles(channel, found_member.destinyUserInfo.membershipType, found_member.destinyUserInfo.membershipId, found_member.destinyUserInfo.LastSeenDisplayName, "3055823");
-			}else{
-				console.log("Check in Antumbra;");
-				var clan1 = new XMLHttpRequest();
-				clan1.open("GET", "https://www.bungie.net/Platform/GroupV2/3858144/Members/", true);
-				clan1.setRequestHeader("X-API-Key", d2apiKey);
-				clan1.onreadystatechange = function(){
-					if(this.readyState === 4 && this.status === 200){
-						var json = JSON.parse(this.responseText);
-					
-						var members1 = json.Response.results;
-						members1.forEach(function(member) { 
-							if(d_member.displayName.startsWith(member.destinyUserInfo.LastSeenDisplayName + " ") || d_member.displayName == member.destinyUserInfo.LastSeenDisplayName){
-								found_member = member;
-							}
-						});
-						
-						if(found_member == null){
-							console.log("Check in Database;");
-							pool.getConnection(function(err, connection) {
-								if (err) throw err; // not connected!
-								
-								var query2 = connection.query('SELECT * FROM members WHERE id = ?',  discord_id, function(err, results, fields) {
-									if (err) throw err;
-									else {
-										if(results.length > 0){
-											results.forEach(function (line){
-												if (line.membershipId == null) channel.send('Пользователь не найден.');
-												else roles(channel, line.membershipType, line.membershipId, d_member.displayName, null);
-											});
-										}else{
-											channel.send('Пользователь не найден.');
-										}
-										connection.release();
-									}
-								});
-							});								
-						}else{
-							console.log(found_member.destinyUserInfo.LastSeenDisplayName);
-							if (sync == true) CreateSync(channel, discord_id, found_member.destinyUserInfo.membershipType, found_member.destinyUserInfo.membershipId, found_member.destinyUserInfo.LastSeenDisplayName);
-							roles(channel, found_member.destinyUserInfo.membershipType, found_member.destinyUserInfo.membershipId, found_member.destinyUserInfo.LastSeenDisplayName, "3858144");
-						}
-					}
-				}
-				clan1.send();
-			}
+	var i = 0;
+	var iteration = function(){
+		if(i < members.length){
+			callback(members[i]);
+			setTimeout(iteration, timeout); 
 		}
 	}
-	clan.send();
+	iteration();
+}
+
+export function ClanTime(message) {
+	ExecuteForEveryMember(1000, function(member){
+		message.channel.send(member);
+	});
 }
