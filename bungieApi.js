6fb94c9fb6e7bacd8269374b50b54a1b8c5fc0b3
Horodep <horodep@yandex.ru>
2020-12-02 05:48:06 +0300

    api GetProfileData

--- bungieApi.js
+++ bungieApi.js
@@ -17,3 +17,12 @@ export async function GetFullMemberData(membershipType, membershipId){
 		console.log("Rejected: " + result);
 	}
 }
+
+export async function GetProfileData(membershipType, membershipId){
+	try{
+		var result = await makeRequestWithPromise('GET', `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=Profiles`);
+		return result.Response.profile;
+	}catch{
+		console.log("Rejected: " + result);
+	}
+}
