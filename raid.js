89df3858d24f413003ec684c647a6649640b6c6f
Horodep <horodep@yandex.ru>
2020-12-18 11:39:11 +0300

    new raid file

---
+++ raid.js
@@ -0,0 +1,25 @@
+import config from "./config.json";
+import { SendPrivateMessage } from "./sendMessage.js";
+
+export function CreateRaid(message, args) {
+}
+
+export function AddRaidMember() {
+
+}
+
+export function RemoveRaidMember() {
+
+}
+
+export function KickRaidMember() {
+
+}
+
+export function CancelRaid() {
+
+}
+
+export function ClearRaidList() {
+
+}
