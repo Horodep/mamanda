794c8168fa85936be3c44ac5991bb1e90cc5fd4e
Horodep <horodep@yandex.ru>
2020-12-23 17:59:44 +0300

    GetMemberData components rework

--- bungieApi.js
+++ bungieApi.js
@@ -1,49 +1,45 @@
 import { makeRequestWithPromise } from "./httpCore.js";
 
-export async function GetClanMembers(clanId){
-	try{
-		var result = await makeRequestWithPromise('GET', `https://www.bungie.net/Platform/GroupV2/${clanId}/Members/`);		
+export async function GetClanMembers(clanId) {
+	try {
+		var result = await makeRequestWithPromise('GET', `https://www.bungie.net/Platform/GroupV2/${clanId}/Members/`);
 		return result.Response.results;
-	}catch{
+	} catch {
 		console.log("Rejected: " + result);
 	}
 }
 
-export async function GetCoreMemberData(membershipType, membershipId){
-	try{
-		var result = await makeRequestWithPromise('GET', `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=Records,Collectibles`);
+async function GetMemberData(membershipType, membershipId, componentsArray) {
+	try {
+		var components = componentsArray.join(",");
+		var result = await makeRequestWithPromise('GET',
+			`https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=${components}`);
 		return result.Response;
-	}catch{
+	} catch {
 		console.log("Rejected: " + result);
 	}
 }
 
-export async function GetFullMemberData(membershipType, membershipId){
-	try{
-		var result = await makeRequestWithPromise('GET', `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=Profiles,Characters,CharacterProgressions,PresentationNodes,Records,Collectibles`);
-		return result.Response;
-	}catch{
-		console.log("Rejected: " + result);
-	}
+export async function GetCoreMemberData(membershipType, membershipId) {
+	return GetMemberData(membershipType, membershipId, ['Records', 'Collectibles']);
 }
 
-export async function GetProfileData(membershipType, membershipId){
-	try{
-		var result = await makeRequestWithPromise('GET', `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=Profiles`);
-		return result.Response.profile;
-	}catch{
-		console.log("Rejected: " + result);
-	}
+export async function GetFullMemberData(membershipType, membershipId) {
+	return GetMemberData(membershipType, membershipId, ['Profiles', 'Characters', 'CharacterProgressions', 'PresentationNodes', 'Records', 'Collectibles']);
 }
 
-export async function GetActivitiesFromApi(membershipType, membershipId, characterId, page, mode){
-	try{
+export async function GetProfileData(membershipType, membershipId) {
+	return GetMemberData(membershipType, membershipId, ['Profiles'])?.profile;
+}
+
+export async function GetActivitiesFromApi(membershipType, membershipId, characterId, page, mode) {
+	try {
 		mode = mode ? mode : "None";
-		var result = await makeRequestWithPromise('GET', 
+		var result = await makeRequestWithPromise('GET',
 			`https://www.bungie.net/Platform/Destiny2/${membershipType}/Account/${membershipId}/Character/${characterId}/Stats/Activities/?mode=${mode}&count=250&page=${page}`);
 		//console.log(`Success! id: ${membershipId} char: ${characterId} page: ${page}`);
 		return result.Response;
-	}catch{
+	} catch {
 		console.log("Rejected: " + result + " id: " + membershipId);
 		return result;
 	}
