b0fa776ff8242c57101b36692338ed508aab034a
Horodep <horodep@yandex.ru>
2020-12-25 17:27:21 +0300

    MessageDelete fix

--- discordEvents/messageDelete.js
+++ discordEvents/messageDelete.js
@@ -9,7 +9,7 @@ export function MessageDelete(message) {
 		if (message.channel.id == config.channels.lfg) return;
 
 		var channelDeleted = message.client.channels.cache.get(config.channels.deleted);
-		channelDeleted.send("<@" + message.author.id + "> в <#"+messageDelete.channel.id+">: " + message.content.split("@").join(""));
+		channelDeleted.send("<@" + message.author.id + "> в <#"+message.channel.id+">: " + message.content.split("@").join(""));
 		for (var value of message.attachments.values()) {
 			channelDeleted.send("Вложение:", { files: [value.proxyURL] });
 		}
