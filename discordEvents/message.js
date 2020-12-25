865928b8a06cdac21f131c753cc602c147f9a5e3
Horodep <horodep@yandex.ru>
2020-12-25 17:04:02 +0300

    no timestamp in log

--- discordEvents/message.js
+++ discordEvents/message.js
@@ -13,7 +13,7 @@ export function Message(message){
 			return;
 		}
 
-		console.log(new Date().toISOString(), (message.member != null ? message.member.displayName : message.author.username), message.content);
+		console.log((message.member != null ? message.member.displayName : message.author.username), message.content);
 		var args = message.content.substring(1).split(' ').filter(item => item);
 		var commandName = args[0];
 
