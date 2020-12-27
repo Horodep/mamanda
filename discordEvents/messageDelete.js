6a90406b651390b734ca6c51b3aeb591aa9fce0f
Horodep <horodep@yandex.ru>
2020-12-27 19:42:26 +0300

    MessageDelete: Cannot read property 'bot' of null Fixes #7

--- discordEvents/messageDelete.js
+++ discordEvents/messageDelete.js
@@ -1,15 +1,15 @@
 import config from "../config.json";
 import { CatchError } from "../catcherror.js";
 
-export function MessageDelete(message) {
+export async function MessageDelete(message) {
 	console.log("messageDeleted");
 	try {
-		if (message.author.bot) return;
+		if (message.author?.bot ?? true) return;
 		if (message.channel.id == config.channels.raids) return;
 		if (message.channel.id == config.channels.lfg) return;
 
 		var channelDeleted = message.client.channels.cache.get(config.channels.deleted);
-		channelDeleted.send("<@" + message.author.id + "> в <#"+message.channel.id+">: " + message.content.split("@").join(""));
+		channelDeleted.send("<@" + message.author?.id + "> в <#"+message.channel.id+">: " + message.content.split("@").join(""));
 		for (var value of message.attachments.values()) {
 			channelDeleted.send("Вложение:", { files: [value.proxyURL] });
 		}
