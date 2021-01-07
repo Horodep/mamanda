2b52fd7729996b6c3145685502791ac665fe14ab
Horodep <horodep@yandex.ru>
2021-01-07 13:07:16 +0300

    .

--- cron/rolesUpdate.js
+++ cron/rolesUpdate.js
@@ -1,8 +1,7 @@
 import Discord from "discord.js";
 import config from "../config.json";
-import { PublishDailyMessage } from "../discordGuildMasterFeatures.js"
 import { SetRoles } from "../clan.js";
-import { CatchError } from "../catcherror.js";
+import { CatchError, FetchDefaultCatchErrorChannel } from "../catcherror.js";
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 client.login(config.credentials.discordApiKey);
