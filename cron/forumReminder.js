8927fa6652fb0a618df1bc6e4aa11393bc93e46a
Horodep <horodep@yandex.ru>
2021-01-07 12:06:50 +0300

    catch error fix

--- cron/forumReminder.js
+++ cron/forumReminder.js
@@ -1,11 +1,13 @@
 import Discord from "discord.js";
 import config from "../config.json";
 import { PublishDailyMessage } from "../discordGuildMasterFeatures.js"
+import { FetchDefaultCatchErrorChannel } from "../catcherror.js";
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 client.login(config.credentials.discordApiKey);
 
 client.on("ready", () => {
+	FetchDefaultCatchErrorChannel(client);
     PublishDailyMessage(client);
     setTimeout(function(){
         client.destroy();
