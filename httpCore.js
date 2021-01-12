85f7cdda5a2da8780049655ee3ecdb788a4ce995
Horodep <horodep@yandex.ru>
2021-01-12 18:33:12 +0300

    auth

--- httpCore.js
+++ httpCore.js
@@ -3,24 +3,28 @@ import { XMLHttpRequest } from "xmlhttprequest";
 import config from "./config.json";
 import { CatchError, CatchBadResponce } from "./catcherror.js";
 
+const accessTokenFileName = '.data/access_token.json';
+
 class AccessToken{
     static #tokenObject = null;
 
-    SetTokenJson(string) {
-        tokenObject = JSON.parse(string);
+    static SetTokenJson(string) {
+        this.#tokenObject = JSON.parse(string);
     }
-    get access_token() {
-        return tokenObject.access_token;
+    static get access_token() {
+        return this.#tokenObject.access_token;
     }
-    get refresh_token() {
-        return tokenObject.refresh_token;
+    static get refresh_token() {
+        return this.#tokenObject.refresh_token;
     }
-    ReadFile(fileName){
-        tokenObject = JSON.parse(fs.readFileSync(fileName));
+    static ReadFile(){
+        var directory = config.credentials.directory ?? "./";
+        this.#tokenObject = JSON.parse(fs.readFileSync(directory + accessTokenFileName));
     }
-    WriteFile(fileName){
-        fs.writeFile(fileName, JSON.stringify(tokenObject), (err) => {
-            if (err)  CatchBadResponce(err);
+    static WriteFile(){
+        var directory = config.credentials.directory ?? "./";
+        fs.writeFile(directory + accessTokenFileName, JSON.stringify(this.#tokenObject), (err) => {
+            if (err) CatchError(err);
         });
     }
 }
@@ -53,42 +57,34 @@ export async function makeRequestWithPromise(method, url, setAuth) {
     });
 }
 
-export async function refreshAuthToken(){
-    AccessToken.ReadFile('access_token.json');
-
-    var body = `grant_type=refresh_token&refresh_token=${AccessToken.refresh_token}&client_id=${config.client_id}&client_secret=${config.client_secret}`;
-
-    var authpost = new XMLHttpRequest();
-    authpost.open("POST", "https://www.bungie.net/Platform/App/OAuth/Token/", true);
-    authpost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    authpost.onreadystatechange = function(){
-        if(this.readyState === 4 && this.status === 200){
-            AccessToken.SetTokenJson(this.responseText);
-            if(AccessToken.access_token){
-                AccessToken.WriteFile('access_token.json');
+async function authRequestWithPromise(body) {
+    return new Promise(function (resolve, reject) {
+        var authpost = new XMLHttpRequest();
+        authpost.open("POST", "https://www.bungie.net/Platform/App/OAuth/Token/", true);
+        authpost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        authpost.onreadystatechange = function(){
+            if(this.readyState === 4 && this.status === 200){
+                AccessToken.SetTokenJson(this.responseText);
+                if(AccessToken.access_token){
+                    AccessToken.WriteFile();
+                }
+                console.log("TOKEN!");
+                resolve(this.responseText);
+            }else{
+                console.log(`    ${this.readyState} ${this.status} ${this.responseText}`);
             }
-            console.log("TOKEN REFRESHED!");
-        }else{
-            console.log(`    ${this.readyState} ${this.status} ${this.responseText}`);
         }
-    }
-    authpost.send(body);
+        authpost.send(body);
+    });
 }
 
-export function newAuthToken(code){
-    var body = `grant_type=authorization_code&code=${code}&client_id=${config.client_id}&client_secret=${config.client_secret}`;
+export async function RefreshAuthToken(){
+    AccessToken.ReadFile();
+    var body = `grant_type=refresh_token&refresh_token=${AccessToken.refresh_token}&client_id=${config.credentials.client_id}&client_secret=${config.credentials.client_secret}`;
+    await authRequestWithPromise(body);
+}
 
-    var authpost = new XMLHttpRequest();
-    authpost.open("POST", "https://www.bungie.net/Platform/App/OAuth/Token/", true);
-    authpost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    authpost.onreadystatechange = function(){
-        if(this.readyState === 4 && this.status === 200){
-            AccessToken.SetTokenJson(this.responseText);
-            if(AccessToken.access_token){
-                AccessToken.WriteFile('access_token.json');
-            }
-            console.log("NEW TOKEN!");
-        }
-    }
-    authpost.send(body);
+export function NewAuthToken(code){
+    var body = `grant_type=authorization_code&code=${code}&client_id=${config.credentials.client_id}&client_secret=${config.credentials.client_secret}`;
+    authRequestWithPromise(body);
 }
