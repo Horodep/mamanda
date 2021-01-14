cebbe25fa88a22c9105b4fade547276ac2ee6f6e
Horodep <horodep@yandex.ru>
2021-01-14 12:57:42 +0300

    Error to sandbox on raid error message generation Fixes #27

--- raid.js
+++ raid.js
@@ -19,7 +19,6 @@ export function CreateRaid(message, args) {
         });
         message.delete();
     } catch (e) {
-        CatchError(e);
         if (e.stack != null) CatchError(e);
         else message.channel.send(
             "Неверный синтаксис: __" + e.message.toLowerCase() + "__\nДолжно быть:\n```!сбор ДД.ММ ЧЧ:ММ активность, комментарии```" +
