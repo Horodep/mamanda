0c680b5da4be052847dfc9defce58714f2d4fe10
Horodep <horodep@yandex.ru>
2021-01-14 12:50:58 +0300

    count rl's once in medals Fixes #25

--- discordCommunityFeatures.js
+++ discordCommunityFeatures.js
@@ -90,7 +90,6 @@ export function GetFullDiscordClanMemberList(guild){
 	var members = [];
 	guild.roles.cache.find(r => r.id == config.roles.guildleader).members.forEach(m => members.push(m));
 	guild.roles.cache.find(r => r.id == config.roles.guildmaster).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.raidleader).members.forEach(m => members.push(m));
 	guild.roles.cache.find(r => r.id == config.roles.guardians[3]).members.forEach(m => members.push(m));
 	guild.roles.cache.find(r => r.id == config.roles.guardians[2]).members.forEach(m => members.push(m));
 	guild.roles.cache.find(r => r.id == config.roles.guardians[1]).members.forEach(m => members.push(m));
