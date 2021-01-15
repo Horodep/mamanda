e85adddc21cc76553a6aa2da02bc3fa33da1d7d8
Horodep <horodep@yandex.ru>
2021-01-15 08:54:11 +0300

    cron ck

---
+++ cron/clanKick.js
@@ -0,0 +1,21 @@
+import Discord from "discord.js";
+import config from "../config.json";
+import { ClanTime } from "../clan.js";
+import { CatchError, FetchDefaultCatchErrorChannel } from "../catcherror.js";
+
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+client.login(config.credentials.discordApiKey);
+
+client.on("ready", () => {
+    try {
+        FetchDefaultCatchErrorChannel(client);
+        var channel = client.channels.cache.get(config.channels.clankick);
+        ClanTime(channel, 7, 'pm');
+        setTimeout(() => {
+            client.destroy();
+            process.exit();
+        }, 180000);
+    } catch (e) {
+        CatchError(e);
+    }
+});
