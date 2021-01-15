f15763b69c1afd31fd6f86724300b29452ab09ef
Horodep <horodep@yandex.ru>
2021-01-15 20:05:07 +0300

    formatting

--- discordCommunityFeatures.js
+++ discordCommunityFeatures.js
@@ -4,89 +4,89 @@ import config from "./config.json";
 export function InviteFriend(message, discordMention) {
 	var topRole = message.member.roles.highest.position;
 	var minRole = message.guild.roles.cache.find(role => role.id == config.roles.guardians[1]).position;
-	
-	if(minRole <= topRole){
-		var discordId = discordMention.replace(/\D/g,'')
+
+	if (minRole <= topRole) {
+		var discordId = discordMention.replace(/\D/g, '')
 		var discordMember = message.guild.members.cache.find(member => member.user.id == discordId);
-		
-		if (discordMember == null){
+
+		if (discordMember == null) {
 			message.channel.send('Страж не найден.');
 			return;
 		}
-		
-		if(discordMember.roles.cache.size == 1){
+
+		if (discordMember.roles.cache.size == 1) {
 			discordMember.roles.add(config.roles.guest);
 			message.channel.send(`Стража <@${discordId}> пустили на сервер.`);
 		}
-	}else{
+	} else {
 		message.channel.send('У вас нет прав на это действие.');
 	};
 }
 
 export function ChangeRegion(message) {
-    var topRole = message.member.roles.highest.position;
+	var topRole = message.member.roles.highest.position;
 	var minRole = message.guild.roles.cache.find(role => role.id == config.roles.afk).position;
-	
-	if(minRole <= topRole){
-		if(message.guild.region == "russia"){
+
+	if (minRole <= topRole) {
+		if (message.guild.region == "russia") {
 			message.guild.setRegion("eu-central");
 			message.channel.send('Регион дискорда сменен на Европу');
-		}else{
+		} else {
 			message.guild.setRegion("russia");
 			message.channel.send('Регион дискорда сменен на Россию');
 		}
-	}else{
+	} else {
 		message.channel.send('У вас нет прав на это действие.');
 	};
 }
 
 export function ChangeChannelCap(message, limit) {
-    if (message.member.voice.channel == null){
+	if (message.member.voice.channel == null) {
 		message.channel.send('Вы не в голосовом канале.');
 		return;
 	}
-	if( message.member.voice.channel.parent.id == config.categories.limited){
+	if (message.member.voice.channel.parent.id == config.categories.limited) {
 		message.channel.send('Вы не можете изменить размер данной комнаты.');
 		return;
 	}
-	try{
+	try {
 		message.member.voice.channel.setUserLimit(limit);
-	}catch{
+	} catch {
 		message.channel.send('Введено некорректное значение.');
-    }
+	}
 }
 
 export function ClanMedalsSummary(channel) {
-    var members = GetFullDiscordClanMemberList(channel.guild);
+	var members = GetFullDiscordClanMemberList(channel.guild);
 
-    var sorted = [];
-    for (let i = 0; i < 36; i++) {
-        sorted[i] = [];
-    }
+	var sorted = [];
+	for (let i = 0; i < 36; i++) {
+		sorted[i] = [];
+	}
 
-    for (let i = 0; i < members.length; i++) {
-        var rolesList = members[i].roles.cache.map(role => role.name).join("");
-        var count = rolesList.match(/💠/g)?.length ?? 0;
-        sorted[count].push(members[i]);
-    }
+	for (let i = 0; i < members.length; i++) {
+		var rolesList = members[i].roles.cache.map(role => role.name).join("");
+		var count = rolesList.match(/💠/g)?.length ?? 0;
+		sorted[count].push(members[i]);
+	}
 
-    const embed = new MessageEmbed()
-        .setAuthor("Aurora's Tilt — " + members.length + " members // увожаемые:")
-        .setColor(0x00AE86)
-        .setFooter("Horobot", "https://cdn.discordapp.com/avatars/543342030768832524/7da47eaca948d9874b66fc5884ca2d00.png")
-        .setTimestamp()
+	const embed = new MessageEmbed()
+		.setAuthor("Aurora's Tilt — " + members.length + " members // увожаемые:")
+		.setColor(0x00AE86)
+		.setFooter("Horobot", "https://cdn.discordapp.com/avatars/543342030768832524/7da47eaca948d9874b66fc5884ca2d00.png")
+		.setTimestamp()
 
-    for (let i = 35; i >= 6; i--) {
-        var text =  sorted[i].map(m => "<@"+m+">").join("\n");
-        var symbol = i < 7 ? "`📘`" : i < 16 ? "`📒`" : i < 24 ? "`📙`" : "`📕`";
+	for (let i = 35; i >= 6; i--) {
+		var text = sorted[i].map(m => "<@" + m + ">").join("\n");
+		var symbol = i < 7 ? "`📘`" : i < 16 ? "`📒`" : i < 24 ? "`📙`" : "`📕`";
 
-        if (text.length > 0) embed.addField(symbol + " " + i + " " + symbol, text, true);
-    }
+		if (text.length > 0) embed.addField(symbol + " " + i + " " + symbol, text, true);
+	}
 
-    channel.send({ embed });
-}				
+	channel.send({ embed });
+}
 
-export function GetFullDiscordClanMemberList(guild){
+export function GetFullDiscordClanMemberList(guild) {
 	var members = [];
 	guild.roles.cache.find(r => r.id == config.roles.guildleader).members.forEach(m => members.push(m));
 	guild.roles.cache.find(r => r.id == config.roles.guildmaster).members.forEach(m => members.push(m));
