5b476f0abd3c987a9333af51e93b44e6f209dd98
Horodep <horodep@yandex.ru>
2021-02-11 19:22:54 +0300

    Fixes !pvpdrop don't work

--- discordGuildMasterFeatures.js
+++ discordGuildMasterFeatures.js
@@ -10,24 +10,24 @@ export async function AsyncShowResetEnglish(channel) {
 	channel.send(response.url);
 }
 
-export function DropPvpRole(guild) {
-	var topPvpRole = guild.roles.find(role => role.id == config.roles.medals.category_first_role.crucible);
-
+export function DropPvpRole(message) {
+	var list = [];
+	var topPvpRole = message.guild.roles.cache.find(role => role.id == config.roles.medals.category_first_role.crucible);
 	for (var rolePosition = topPvpRole.position - 1; rolePosition > topPvpRole.position - 7; rolePosition--) {
-		var role = guild.roles.find(role => role.position == rolePosition);
-		var list = [];
-		role.members.forEach(member => { list.push(member); });
-
-		var i = 0;
-		var dropRole = function () {
-			if (i < list.length) {
-				list[i].roles.remove(role);
-				i++;
-				setTimeout(dropRole, 1000);
-			}
-		}
-		dropRole();
+		var role = message.guild.roles.cache.find(role => role.position == rolePosition);
+		role.members.forEach(member => { list.push({member: member, role: role}); });
 	}
+	var i = 0;
+	var dropRole = function () {
+		if (i < list.length) {
+			list[i].member.roles.remove(list[i].role);
+			i++;
+			setTimeout(dropRole, 1000);
+		}else{
+			message.channel.send("Пвп роли сняты");
+		}
+	}
+	dropRole();
 }
 
 export function GiveForumRole(message) {
