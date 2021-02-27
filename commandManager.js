12da2a7ba3fc8769d1a69e0b2d74114d54285594
Horodep <horodep@yandex.ru>
2021-02-28 01:34:42 +0300

    queue

--- commandManager.js
+++ commandManager.js
@@ -10,7 +10,7 @@ import { AsyncGetClanMemberOnlineTime } from "./clanMember.js";
 import { CatchError } from "./catcherror.js";
 import { InviteFriend, ChangeChannelCap, ChangeRegion, ClanMedalsSummary, ShowLegendarySectors } from "./discordFeatures/discordCommunityFeatures.js";
 import { DropPvpRole, GiveForumRole, SaveForumLinkAndPublish, SetMaximumTriumphsScore, ShowNewbieList, AsyncShowResetEnglish } from "./discordFeatures/discordGuildMasterFeatures.js"
-import { AsyncShowQueueList, AsyncShowQueueReqestsList } from "./queue.js"
+import { AsyncShowQueueList, AsyncShowQueueReqestsList } from "./discordFeatures/queue.js"
 import { SendCustomMessage, SendPrivateMessageByRole } from "./sendMessage.js";
 import { ClearRaidList, CreateRaid, ForcedAddRaidMember, ForcedRemoveRaidMember, AsyncGetPlannedRaids } from "./raid.js"
 import { AsyncDrawXur } from "./drawing/drawXur.js";
