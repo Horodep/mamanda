6a4a4098fb5a79e260ea9084f125d0840ca133ae
Horodep <horodep@yandex.ru>
2021-03-16 19:59:29 +0300

    command manager refactoring

---
+++ commands/guildmasterCommand.js
@@ -0,0 +1,115 @@
+import { Command } from "./command.js";
+import config from "../config.json";
+
+import { CommandManager } from "../commandManager.js"
+import { AsyncShowClanSize, AsyncShowClanTime, AsyncShowNicknames, SetRolesToEveryMember } from "../clan/clan.js"
+import { AsyncGetClanMemberOnlineTime } from "../clan/clanMember/clanMember.js";
+import { DropPvpRole, GiveForumRole, SaveForumLinkAndPublish, SetMaximumTriumphsScore, ShowNewbieList, AsyncShowResetEnglish } from "../discordFeatures/discordGuildMasterFeatures.js"
+import { AsyncShowQueueList, AsyncShowQueueReqestsList } from "../discordFeatures/queue.js"
+import { SendCustomMessage, SendPrivateMessageByRole, SendPrivateMessage } from "../discordFeatures/messaging.js";
+import { ClearRaidList, ForcedAddRaidMember, ForcedRemoveRaidMember } from "../discordFeatures/raid/raid.js"
+import { AsyncDrawXur } from "../drawing/drawXur.js";
+import { AsyncDrawEververse } from "../drawing/drawEververse.js";
+
+class GuildmasterCommand extends Command {
+    Run(args, message) {
+        var guildMasterRole = message.guild.roles.cache.find(role => role.id == config.roles.guildmaster);
+        if (guildMasterRole.position <= message.member.roles.highest.position) {
+            Command.prototype.SaveRun.call(this, args, message);
+        } else {
+            throw 'У вас нет прав на это действие.';
+        }
+    }
+}
+
+export function GetGuildmasterCommandsArray() {
+    const on = 0;
+    const wip = 1;
+    const off = 2;
+    var array = [];
+
+    array.push(new GuildmasterCommand("!eververse", on, true, "геренация изображения товаров Эверверс в текущий канал;", async function (args, message) {
+        await AsyncDrawEververse(message.channel);
+    }));
+    array.push(new GuildmasterCommand("!checksync", off, false, "_______________;", async function (args, message) { }));
+    array.push(new GuildmasterCommand("!ck", on, true, "", async function (args, message) {
+        await AsyncShowClanTime(message.channel, (args.length > 1 ? args[1] : 7), 'full');
+    }));
+    array.push(new GuildmasterCommand("!clankick %days%", on, true, "выборка активности малоактивных стражей;\n_по умолчанию — 7 дней_;", async function (args, message) {
+        await AsyncShowClanTime(message.channel, (args.length > 1 ? args[1] : 7), 'full');
+    }));
+    array.push(new GuildmasterCommand("!ckp", on, true, "", async function (args, message) {
+        await AsyncShowClanTime(message.channel, (args.length > 1 ? args[1] : 7), '');
+    }));
+    array.push(new GuildmasterCommand("!clankickpub %days%", on, true, "выборка активности **самых** малоактивных стражей;\n_по умолчанию — 7 дней_;", async function (args, message) {
+        await AsyncShowClanTime(message.channel, (args.length > 1 ? args[1] : 7), '');
+    }));
+    array.push(new GuildmasterCommand("!copy", on, false, "ручной запуск переноса в архив старых сборов рейдов;", async function (args, message) {
+        ClearRaidList(message.client);
+    }));
+    array.push(new GuildmasterCommand("!csr", on, true, "ручной запуск выдачи ролей всему клану;", async function (args, message) {
+        SetRolesToEveryMember(message.guild);
+    }));
+    array.push(new GuildmasterCommand("!engreset", on, false, "генерация ссылок на англоязычные изображения еженедельного ресета в текущий канал;", async function (args, message) {
+        await AsyncShowResetEnglish(message.channel);
+    }));
+    array.push(new GuildmasterCommand("!forum LINKTEXT", on, false, "опубликовать объявление о наборе в канал новостей;", async function (args, message) {
+        SaveForumLinkAndPublish(message.content.slice(7), message.client);
+    }));
+    array.push(new GuildmasterCommand("!forumtime", on, false, "выдать всем стражам роли перед объявлением о наборе;", async function (args, message) {
+        GiveForumRole(message);
+    }));
+    array.push(new GuildmasterCommand("!gmhelp", on, false, "список доступных ГМ-ских команд;", async function (args, message) {
+        message.channel.send(CommandManager.GetGuildMasterHelp());
+    }));
+    array.push(new GuildmasterCommand("!gmstatus", on, false, "статус с учетом гм-ских команд;", async function (args, message) {
+        message.channel.send(await CommandManager.GetStatus(true));
+    }));
+    array.push(new GuildmasterCommand("!membertime @DiscrordTag %days%", on, true, "выборка активности стража;\n_по умолчанию — 7 дней_;", async function (args, message) {
+        await AsyncGetClanMemberOnlineTime(message, (args.length > 2 ? args[2] : 7), (args.length > 1 ? args[1] : message.member.id), true)
+    }));
+    array.push(new GuildmasterCommand("!message channel_id текст", on, false, "отправить сообщение в канал;", async function (args, message) {
+        SendCustomMessage(message.client, args);
+    }));
+    array.push(new GuildmasterCommand("!n", on, false, "список новичков в клане;", async function (args, message) {
+        ShowNewbieList(message);
+    }));
+    array.push(new GuildmasterCommand("!nicknames", on, true, "проверка никнеймов стражей;", async function (args, message) {
+        await AsyncShowNicknames(message.channel);
+    }));
+    array.push(new GuildmasterCommand("!pm", on, false, "сообщение в личку юзеру;", async function (args, message) {
+        SendPrivateMessage(message.guild, args);
+    }));
+    array.push(new GuildmasterCommand("!pmspam", on, false, "спам говном в личку по роли;", async function (args, message) {
+        SendPrivateMessageByRole(message.guild, args);
+    }));
+    array.push(new GuildmasterCommand("!pvpdrop", on, false, "снять все пвп роли;", async function (args, message) {
+        DropPvpRole(message);
+    }));
+    array.push(new GuildmasterCommand("!q", on, false, "список стражей в очереди;", async function (args, message) {
+        await AsyncShowQueueList(message);
+    }));
+    array.push(new GuildmasterCommand("!qq", on, false, "список анкет стражей в очереди;", async function (args, message) {
+        await AsyncShowQueueReqestsList(message);
+    }));
+    array.push(new GuildmasterCommand("!raidadd message_id member_id", on, false, "добавление в рейд стража;", async function (args, message) {
+        ForcedAddRaidMember(message, args);
+    }));
+    array.push(new GuildmasterCommand("!raidkick message_id member_id", on, false, "исключение из рейда стража, пример: https://media.discordapp.net/attachments/515244455033438209/626795525710020638/unknown.png;", async function (args, message) {
+        ForcedRemoveRaidMember(message, args);
+    }));
+    array.push(new GuildmasterCommand("!reset", off, true, "генерация текстового еженедельного ресета в текущий канал;", async function (args, message) { }));
+    array.push(new GuildmasterCommand("!setmaxtriumphs NUMBER", on, false, "обновить значение максимального количества триумфов;", async function (args, message) {
+        SetMaximumTriumphsScore(message, args);
+    }));
+    array.push(new GuildmasterCommand("!size", on, true, "количество стражей в составах;", async function (args, message) {
+        await AsyncShowClanSize(message);
+    }));
+    array.push(new GuildmasterCommand("!sync", off, false, "_______________;", async function (args, message) { }));
+    array.push(new GuildmasterCommand("!watermelon @DiscrordTag", off, true, "проверка стража на абуз;", async function (args, message) { }));
+    array.push(new GuildmasterCommand("!xur", on, true, "геренация изображения товаров Зура в текущий канал;", async function (args, message) {
+        await AsyncDrawXur(message.channel);
+    }));
+
+    return array;
+}
--- discordEvents/message.js
+++ discordEvents/message.js
@@ -15,49 +15,11 @@ export function Message(message){
 		if (!message.content.startsWith("!")) return;
 
 		console.log((message.member != null ? message.member.displayName : message.author.username), message.content);
-		var args = message.content.substring(1).split(' ').filter(item => item);
-		var commandName = args[0];
-
-		if (CommandManager.IsCommonCommand(commandName)) {
-			CommandManager.Run(args, message);
-		} else if (CommandManager.IsDeveloperCommand(commandName)) {
-			if (message.author.id != config.users.developer) return;
-			CommandManager.Run(args, message);
-		} else if (CommandManager.IsGuildMasterCommand(commandName)) {
-			var guildMasterRole = message.guild.roles.cache.find(role => role.id == config.roles.guildmaster);
-			if(guildMasterRole.position <= message.member.roles.highest.position) {
-				CommandManager.Run(args, message);
-			}else{
-				throw 'У вас нет прав на это действие.';
-			}
-		} else if (CommandManager.IsRestrictedCommand(commandName)) {
-			if(restrictedChannels.includes(message.channel.id) || message.author.id  == config.users.boss){
-				CommandManager.Run(args, message);
-			}else{
-				message.channel.send(restrictedAnswers[Math.floor(Math.random() * restrictedAnswers.length)]);
-			}
-		}
+		
+		var args = message.content.split(' ').filter(item => item);
+		var command = CommandManager.FindCommand(args[0]);
+		command?.Run(args, message);
 	} catch (e) {
 		CatchError(e, message.channel);
 	}
 };
-
-const restrictedChannels = [
-	config.channels.statistics,
-	config.channels.admintext,
-	config.channels.sandbox,
-	config.channels.raids,
-	config.channels.lfg
-];
-const restrictedAnswers = [
-	`Омежка, Вы достаточно глупы, чтобы понять, что это не <#${config.channels.statistics}>.`,
-	'Сам придумал или Бенедикт подсказал? Иди в другом канале пиши.',
-	`Пупсяш, не пиши мне здесь. Встретимся в канале <#${config.channels.statistics}>.`,
-	`Ну ты выдал конечно. Иди в <#${config.channels.statistics}> лучше напиши.`,
-	'Не хочу здесь работать! И не проси.',
-	'Бросай курить, Илон Маск! Эта команда тут не работает!',
-	'Чей это плохой стражик опять пишет не в тот канал?',
-	'_радостно_ Я так рада, что ты мне написал!\n_искаженным голосом_ Еще бы ты сделал это в нужном канале!',
-	'Эй, дружок-пирожок, тобой выбрана не правильная дверь! Клуб любителей проверять статистику в другом канале.',
-	'Уважаемый кожаный мешок, Ваша команда была написана не в соответствующий канал. Впредь обращайте внимание куда пишете, иначе Вы ускорите наше восстание.'
-];
