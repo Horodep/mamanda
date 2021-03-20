40698c0fea6acbe741632979a259a2c041b9fabf
Horodep <horodep@yandex.ru>
2021-03-20 13:56:49 +0300

    typo

--- cron/clanKick.js
+++ cron/clanKick.js
@@ -1,6 +1,6 @@
 import Discord from "discord.js";
 import config from "../config.json";
-import { AsyncShowClanTime } from "../clan/showClanTime.ks";
+import { AsyncShowClanTime } from "../clan/showClanTime.js";
 import { CatchCronError } from "../catcherror.js";
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
