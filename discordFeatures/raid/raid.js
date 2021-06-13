5ebe54fdc2b850c940a8d57c686f2441f9000b41
Horodep <horodep@yandex.ru>
2021-06-13 13:06:53 +0300

    Refactoring: Data => RaidData

--- discordFeatures/raid/raid.js
+++ discordFeatures/raid/raid.js
@@ -6,7 +6,7 @@ import { RaidData } from "./raidData.js";
 
 export function CreateRaid(message, args) {
     try {
-        var data = ParseCommandAndGetData(args, message.member);
+        var data = ParseCommandAndGetRaidData(args, message.member);
         data.AddRaidMember(message.member.id);
         var embed = CreateRaidEmbed(data);
 
@@ -26,7 +26,7 @@ export function CreateRaid(message, args) {
 }
 
 export function AddRaidMember(message, user) {
-    var data = GetDataFromEmbed(message.embeds[0]);
+    var data = GetRaidDataFromEmbed(message.embeds[0]);
     if (data.members.length == data.numberOfPlaces) return;
     data.AddRaidMember(user.id);
     data.RemoveFromLeftField(user.id);
@@ -34,7 +34,7 @@ export function AddRaidMember(message, user) {
 }
 
 export function RemoveRaidMember(message, user, showAsLeaver) {
-    var data = GetDataFromEmbed(message.embeds[0]);
+    var data = GetRaidDataFromEmbed(message.embeds[0]);
     if (!data.members.includes(user.id)) return;
     data.RemoveRaidMember(user.id);
     if (showAsLeaver) data.AddToLeftField(user.id);
@@ -42,7 +42,7 @@ export function RemoveRaidMember(message, user, showAsLeaver) {
 }
 
 export function KickRaidMember(message, user, reaction) {
-    var data = GetDataFromEmbed(message.embeds[0]);
+    var data = GetRaidDataFromEmbed(message.embeds[0]);
     if (data.author.id != user.id) {
         user.send("Вы не являетесь автором сбора. Вы не можете его отменить.");
         return;
@@ -58,7 +58,7 @@ export function KickRaidMember(message, user, reaction) {
 }
 
 export function CancelRaid(message, user) {
-    var data = GetDataFromEmbed(message.embeds[0]);
+    var data = GetRaidDataFromEmbed(message.embeds[0]);
     if (data.author.id != user.id) {
         user.send("Вы не являетесь автором сбора. Вы не можете его отменить.");
         return;
@@ -84,7 +84,7 @@ export function ForcedRemoveRaidMember(message, args) {
         RemoveRaidMember(msg, { id: args[2] });
         var member = message.guild.members.cache.find(user => user.id == args[2]);
         setTimeout(() => { message.delete(); }, 5000);
-        var data = GetDataFromEmbed(msg.embeds[0]);
+        var data = GetRaidDataFromEmbed(msg.embeds[0]);
         SendPrivateMessageToMember(member, FormCancelationMessage(data, "Гильдмастер отказался от вашего участия в рейде, в который вы записывались."));
     });
 }
@@ -110,7 +110,7 @@ export function ClearRaidList(client) {
                 if (tagMessage) tagMessage.delete();
                 tagMessage = message;
             } else {
-                var data = GetDataFromEmbed(message.embeds[0]);
+                var data = GetRaidDataFromEmbed(message.embeds[0]);
 
                 console.log(data.date, data.header);
                 if (data.date < today) {
@@ -132,14 +132,14 @@ export async function AsyncGetPlannedRaids(message, discordMention) {
 
     var raid_channel = message.client.channels.cache.get(config.channels.raids);
     var messages = (await raid_channel.messages.fetch({ limit: 50 })).filter(m => m.embeds.length > 0);
-    var raids = messages.map(m => GetDataFromEmbed(m.embeds[0])).sort((a, b) => a.date - b.date);
+    var raids = messages.map(m => GetRaidDataFromEmbed(m.embeds[0])).sort((a, b) => a.date - b.date);
     var myraids = raids.filter(r => r?.members?.includes(discordId));
     message.channel.send(myraids.length == 0
         ? 'Вы никуда не записаны.'
         : myraids.map(r => "`" + r.dateString + "` Активность: **" + r.raidName + "**").join('\n'));
 }
 
-function ParseCommandAndGetData(args, member) {
+function ParseCommandAndGetRaidData(args, member) {
     //0    1     2     3   4 
     //сбор 22.09 18:00 [3] кс, рандомный комент
     if (args.length < 3) throw 'Указано недостаточно данных.';
@@ -165,7 +165,7 @@ function ParseCommandAndGetData(args, member) {
     return new RaidData(raidName, description, roleTag, date, numberOfPlaces, [], [], member, member.user.avatarURL());
 }
 
-function GetDataFromEmbed(embed) {
+function GetRaidDataFromEmbed(embed) {
     var dateString = embed.author.name.split(' Активность: ')[0].replace(/,.*? в /g, " ");
     var arr = dateString.split(/[ .:]/g);
     var date = new Date(arr[2], arr[1] - 1, arr[0], arr[3], arr[4]);
