5e43b6f8e0fa1ae48674790e1c0061dac31e3e05
Horodep <github@horodep.me>
2022-06-07 19:16:51 +0000

    vyakban forgotten embed

--- discordFeatures/vyakManager.js
+++ discordFeatures/vyakManager.js
@@ -31,11 +31,12 @@ function CountDeltaTime(delta) {
 }
 
 function CreateVyakbanEmbed(member, now, limit, reason) {
-    return new MessageEmbed()
-        .setAuthor({ name: member.displayName })
-        .setColor(0xDE0C00)
-        .setDescription(":information_source: **" + reason + "**\n" + no + " " + now + "\n" + yes + " " + limit)
-        .setFooter({ text: member.id + " " + limit.getTime(), iconURL: 'https://cdn.discordapp.com/avatars/564870880853753857/127385781e26e7dcfdbe312de1843ddf.png' })
+    return { embeds: [ 
+        new MessageEmbed()
+            .setAuthor({ name: member.displayName })
+            .setColor(0xDE0C00)
+            .setDescription(":information_source: **" + reason + "**\n" + no + " " + now + "\n" + yes + " " + limit)        
+    ] };
 }
 
 export function CheckVyakbansLimitations(client) {
