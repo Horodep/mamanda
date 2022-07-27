f0d21afc0c355f6005df018a259cb16329868db4
Horodep <github@horodep.me>
2022-07-27 17:25:03 +0000

    GetDiscordClanMemberList changed role based logic

--- discordFeatures/getDiscordClanMemberList.js
+++ discordFeatures/getDiscordClanMemberList.js
@@ -8,13 +8,7 @@ export async function AsyncGetDiscordClanMemberList(guild) {
 
 export function GetDiscordClanMemberList(guild) {
 	var members = [];
-	guild.roles.cache.find(r => r.id == config.roles.guildleader).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.guildmaster).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.guardians[3]).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.guardians[2]).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.guardians[1]).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.guardians[0]).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.newbie).members.forEach(m => members.push(m));
-	guild.roles.cache.find(r => r.id == config.roles.afk).members.forEach(m => members.push(m));
+	guild.roles.cache.find(r => r.id == config.roles.clans[1]).members.forEach(m => members.push(m));
+	guild.roles.cache.find(r => r.id == config.roles.clans[0]).members.forEach(m => members.push(m));
 	return members;
 }
