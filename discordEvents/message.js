ff5445984ef19badfff98b7fca25909399944f1d
Horodep <github@horodep.me>
2022-10-02 11:32:46 +0000

    new message channel type processing

--- discordEvents/message.js
+++ discordEvents/message.js
@@ -6,21 +6,25 @@ export function Message(message){
 	try {
 		if (message.author.bot) return;
 		
-		if (message.channel.type != "GUILD_TEXT") {
-			var channel_sandbox = message.client.channels.cache.get(config.channels.sandbox);
-			channel_sandbox.send("**" + message.author.username + "** написал в ЛС:\n" + message.content);
-			return;
-		}
+		switch(message.channel.type){
+			case "DM":
+				var channel_sandbox = message.client.channels.cache.get(config.channels.sandbox);
+				channel_sandbox.send("**" + message.author.username + "** написал в ЛС:\n" + message.content);
+				return;
+			case "GUILD_TEXT":
+				if (!message.content.startsWith("!")) return;
 
-		if (!message.content.startsWith("!")) return;
-
-		if (config.credentials.is_production == (config.channels.development == message.channel.id)) return;
-
-		console.log((message.member != null ? message.member.displayName : message.author.username), message.content);
+				if (config.credentials.is_production == (config.channels.development == message.channel.id)) return;
 		
-		var args = message.content.split(' ').filter(item => item);
-		var command = CommandManager.FindCommand(args[0]);
-		command?.Run(args, message);
+				console.log((message.member != null ? message.member.displayName : message.author.username), message.content);
+				
+				var args = message.content.split(' ').filter(item => item);
+				var command = CommandManager.FindCommand(args[0]);
+				command?.Run(args, message);
+				return;
+			default:
+				return;
+		}
 	} catch (e) {
 		CatchError(e, message.channel);
 	}
